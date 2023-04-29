d080723c9084d4910635d316d8e2d053dfcb48b2
Ежиков Никита Алексеевич <yukio_wtnb@mail.ru>
2023-04-29 13:50:22 +0300

    lab_6

--- Task06/Task06_1/src/MaxPriceFilter.php
+++ Task06/Task06_1/src/MaxPriceFilter.php
@@ -4,7 +4,21 @@ namespace App;
 
 class MaxPriceFilter implements ProductFilteringStrategy
 {
+    private $maxPrice;
+
+    public function __construct($maxPrice)
+    {
+        $this->maxPrice = $maxPrice;
+    }
     // ===================================
     //@TODO Реализовать стратегию фильтрации по цене товара
     // ===================================
+    public function filter(Product $product): bool
+    {
+        if ($product->sellingPrice) {
+            return $this->maxPrice >= $product->sellingPrice;
+        } else {
+            return $this->maxPrice >= $product->listPrice;
+        }
+    }
 }
